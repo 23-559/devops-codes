02bb94d5d63be5bdc55b4c1f9c1627df774f7192
NETHAJI <sunkarinethaji@gmail.com>
2025-09-09 15:28:22 +0530

    Update msg.c

--- msg.c
+++ msg.c
@@ -1,49 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
+#include<stdio.h>
 
-// FIFO includes
-#include <sys/stat.h>
-#include <fcntl.h>
-
-// Message Queue includes
-#include <sys/ipc.h>
-#include <sys/msg.h>
-
-// Shared Memory includes
-#include <sys/shm.h>
-
-#define MSG_SIZE 100
-#define SHM_KEY 1234
-
-// Message structure for message queue
-struct msg_buffer {
-    long msg_type;
-    char msg_text[MSG_SIZE];
-};
-
-int main() {
-    pid_t pid;
-    char msg[] = "Hello IPC!";
-    char buffer[MSG_SIZE];
- printf("\n--- MESSAGE QUEUE ---\n");
-    key_t key = ftok("progfile", 65);
-    int msgid = msgget(key, 0666 | IPC_CREAT);
-    pid = fork();
-    if (pid > 0) {
-        struct msg_buffer msgbuf;
-        msgbuf.msg_type = 1;
-        strcpy(msgbuf.msg_text, msg);
-        msgsnd(msgid, &msgbuf, sizeof(msgbuf.msg_text), 0);
-        wait(NULL);
-    } else {
-        struct msg_buffer msgbuf;
-        msgrcv(msgid, &msgbuf, sizeof(msgbuf.msg_text), 1, 0);
-        printf("Child received via MSG QUEUE: %s\n", msgbuf.msg_text);
-        msgctl(msgid, IPC_RMID, NULL);
-        exit(0);
-    }
-
-
+int main(){
+    int x;
+    printf("bcws: ");
+    scanf("%d", &x);
+    printf("%d",x);
+    return 0;
+}
